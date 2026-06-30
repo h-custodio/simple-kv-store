ac6b39e8bca8c0fe08fb1207d577e1062dd65375
h-custodio <hprince0814@gmail.com>
2026-06-30 00:02:59 -0700

    refactored to return Result as structured output and made minor fixes

--- commands.py
+++ commands.py
@@ -1,53 +1,58 @@
 
-def execute(store, command, args):
+# Consider dispatch table later on
+
+def execute(store, command, args) -> Result:
+
     if command == "SET" and len(args) == 2:
         store.set(args[0], args[1])
-        return "OK"
+        return Result(success=True, value="Success", appendable=True)
 
-    elif command == "GET" and len(args) == 1:
-        exist = store.show()
+    if command == "GET" and len(args) == 1:
+        if not store.exists(args[0]):
+            return Result(success=False, error="this key does not exist")
 
-        if args[0] not in exist:
-            print("this key does not exist")
-            return None
-        else:
-            return store.get(args[0])
+        return Result(success=True, value=store.get(args[0]))
 
-    elif command == "DEL" and len(args) == 1:
-        exist = store.show()
+    if command == "DEL" and len(args) == 1:
+        if not store.exists(args[0]):
+            return Result(success=False, error="this key does not exist")
+        
+        store.delete(args[0])
+        return Result(success=True, value=args[0] + " has been deleted", appendable=True)
 
-        if args[0] not in exist:
-            print("this key does not exist")
-            return None
-        else:
-            store.delete(args[0])
-            return args[0] + " has been deleted"
+    if command == "SHOW":
+            return Result(success=True, value=store.show())
 
-    elif command == "QUIT":
-        raise SystemExit("Stopping program")
+    if command == "PING":
+        return Result(success=True, value="Pong")
     
-    elif command == "SHOW":
-            return store.show()
-
-    elif command == "PING":
-        print("PONG")
+    if command == "QUIT":
+        raise SystemExit("Stopping program")
         
     else:
-        print("command unknown")
-        return None 
+        return Result(success=False, error="command unknown") 
+    
 
-def parse(string):
+def parse(string: str):
     if string == "":
+        print("this string cannot be empty")
         return
-    else:
-        return string.split()
+    
+    return string.split()
     
 
 # move into different file as system scales
 
 class Result:
-    def __init__(self):
-        success: bool
-        value: str | None = None
-        error: str | None = None
-        appendable: bool = None
+    def __init__(self, 
+        success: bool, 
+        value: any | None = None, 
+        error: str | None = None, 
+        appendable: bool = False):
+
+        self.success = success
+        self.value = value
+        self.error = error
+        self.appendable = appendable
+
+
--- repl_kv_store.py
+++ repl_kv_store.py
@@ -1,7 +1,8 @@
 from kv_store import KVStore
-import commands as c
+import commands as cmd
 
 # Issues to address: parser logic is still fragile
+
 # YOU ARE WORKING IN THE PERSISTENCE BRANCH RN #
 
 # Consider adding this later on
@@ -17,7 +18,7 @@ def repl(store):
         print("$ ", end="")
 
         input_string = input("input: ")
-        input_string = c.parse(input_string)
+        input_string = cmd.parse(input_string)
 
         if input_string is None:
             print("input cannot be empty")
@@ -26,9 +27,12 @@ def repl(store):
         command = input_string[0].upper()
         args = input_string[1:]
 
-        result = c.execute(store, command, args)
-        if result is not None:
-            print(result)
+        result = cmd.execute(store, command, args)
+        if not result.success:
+            print(result.error)
+        else:
+            print(result.value)
+
 
 
 def main():
